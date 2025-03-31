4bb9cb9d66f16b1f732e9fc5f54b9d46f20c5c39
d_kaplan <d.kaplan@just-ai.com>
2025-03-31 23:21:13 +0300

    added core endpoints and exception handler

--- src/main/kotlin/ru/kaplaan/kcloak/domain/exception/UserNotFoundException.kt
+++ src/main/kotlin/ru/kaplaan/kcloak/domain/exception/UserNotFoundException.kt
@@ -1,6 +1,12 @@
 package ru.kaplaan.kcloak.domain.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.security.oauth2.core.OAuth2Error
+
 
 class UserNotFoundException(
-    override val message: String,
-) : RuntimeException(message)
+    description: String,
+) : OAuth2Exception(
+    oauth2Error = OAuth2Error("not found", description, null),
+    httpStatus = HttpStatus.BAD_REQUEST
+)
